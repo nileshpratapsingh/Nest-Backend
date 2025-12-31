147c61958185eaf846b6979810b411fc82485d64
Nilesh Pratap Singh <nileshpratap190902@gmail.com>
2026-01-01 00:38:53 +0530

    Genrated resources

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,12 @@ import { AppService } from './app.service';
 import { ProductModule } from './product/product.module';
 import { UserModule } from './user/user.module';
 import { AdminModule } from './admin/admin.module';
+import { CartModule } from './cart/cart.module';
+import { PaymentModule } from './payment/payment.module';
+import { OrderModule } from './order/order.module';
 
 @Module({
-  imports: [ProductModule, UserModule, AdminModule],
+  imports: [ProductModule, UserModule, AdminModule, CartModule, PaymentModule, OrderModule],
   controllers: [AppController],
   providers: [AppService],
 })
