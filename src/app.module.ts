861821bb7c8461aaa1659360bd92459d3a753180
Nilesh Pratap Singh <nileshpratap190902@gmail.com>
2026-01-01 00:37:30 +0530

    Genrated resources

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProductModule } from './product/product.module';
+import { UserModule } from './user/user.module';
+import { AdminModule } from './admin/admin.module';
 
 @Module({
-  imports: [],
+  imports: [ProductModule, UserModule, AdminModule],
   controllers: [AppController],
   providers: [AppService],
 })
