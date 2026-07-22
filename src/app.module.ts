a190db03e27d1eea5af6b184980e121444ed0316
Nilesh Pratap Singh <nileshpratap190902@gmail.com>
2026-07-22 18:11:11 +0530

    Other services configuration.

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,10 +7,33 @@ import { AdminModule } from './admin/admin.module';
 import { CartModule } from './cart/cart.module';
 import { PaymentModule } from './payment/payment.module';
 import { OrderModule } from './order/order.module';
+import { MessageModule } from './message/message.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [ProductModule, UserModule, AdminModule, CartModule, PaymentModule, OrderModule],
+    imports: [
+        ConfigModule.forRoot({
+            isGlobal: true,
+            envFilePath: '.env',
+        }),
+        MongooseModule.forRootAsync({
+            inject: [ConfigService],
+            useFactory: (configService: ConfigService) => {
+                const uri = configService.get<string>('MONGO_URI');
+                if (!uri) throw new Error('MONGO_URI is not defined');
+                return { uri };
+            },
+        }),
+        ProductModule,
+        UserModule,
+        AdminModule,
+        CartModule,
+        PaymentModule,
+        OrderModule,
+        MessageModule,
+    ],
   controllers: [AppController],
-  providers: [AppService],
+    providers: [AppService],
 })
 export class AppModule {}
