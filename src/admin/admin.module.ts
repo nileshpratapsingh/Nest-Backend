8fad447bc8513eeb656ca9bccb6bc28f72725102
Nilesh Pratap Singh <nileshpratap190902@gmail.com>
2026-07-22 17:47:39 +0530

    Add configurations for the service.

--- src/admin/admin.module.ts
+++ src/admin/admin.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AdminService } from './admin.service';
 import { AdminController } from './admin.controller';
-
+import { MongooseModule } from '@nestjs/mongoose';
+import { User, UserSchema } from '@schemas/user.schema';
 @Module({
-  controllers: [AdminController],
-  providers: [AdminService],
+    imports: [
+        MongooseModule.forFeature([
+            { name: User.name, schema: UserSchema },
+        ]),
+    ],
+    controllers: [AdminController],
+    providers: [AdminService],
 })
 export class AdminModule {}
