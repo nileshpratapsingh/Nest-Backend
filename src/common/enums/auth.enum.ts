ef9f8aad1c35cd36dd260239b16b45a2c7bcb7ef
Nilesh Pratap Singh <nileshpratap190902@gmail.com>
2026-07-22 18:08:58 +0530

    Auth Enumerations.

---
+++ src/common/enums/auth.enum.ts
@@ -0,0 +1,12 @@
+export enum Gender {
+  MALE = 'Male',
+  FEMALE = 'Female',
+  OTHERS = 'Others',
+}
+
+export enum Role {
+  USER = 'user',
+  ADMIN = 'admin',
+  SUPER_ADMIN = 'super-admin',
+}
+
