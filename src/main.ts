7bed457477620f92ee42f25e88de41e97ce90773
Nilesh Pratap Singh <nileshpratap190902@gmail.com>
2026-07-22 18:13:21 +0530

    MongoDB configuration.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+    const app = await NestFactory.create(AppModule);
+    const Config = app.get(ConfigService);
+
+    console.log('process.env.MONGO_URI =', Config.get<string>('MONGO_URI'));
+    app.useGlobalPipes(
+        new ValidationPipe({
+            whitelist: true,
+            transform: true,
+            transformOptions: {
+                enableImplicitConversion: true,
+            },
+        }),
+    );
+    app.enableShutdownHooks();
+    await app.listen( Config.get<string>('PORT') ?? 3000);
 }
 bootstrap();
