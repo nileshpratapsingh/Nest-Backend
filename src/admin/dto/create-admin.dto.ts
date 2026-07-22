7d43b1e34effbdf92cc2f796718ef0fe1578d6d4
Nilesh Pratap Singh <nileshpratap190902@gmail.com>
2026-07-22 17:50:08 +0530

    Admin data transfer object.

--- src/admin/dto/create-admin.dto.ts
+++ src/admin/dto/create-admin.dto.ts
@@ -1,1 +1,65 @@
-export class CreateAdminDto {}
+import {
+  IsString,
+  IsNotEmpty,
+  IsOptional,
+  IsEmail,
+  IsDateString,
+  IsBoolean,
+  MinLength,
+  Matches,
+} from 'class-validator';
+import { Type } from 'class-transformer';
+import { createAddressDto } from '../../common/dtos/create-address.dto';
+
+export class CreateAdminDto {
+  @IsOptional()
+  @IsString()
+  profileImage?: string;
+
+  @IsOptional()
+  @IsString()
+  role?: string;
+
+  @IsString()
+  @IsNotEmpty()
+  firstName!: string;
+
+  @IsOptional()
+  @IsString()
+  middleName?: string;
+
+  @IsString()
+  @IsNotEmpty()
+  lastName!: string;
+
+  @IsString()
+  @Matches(/^[0-9]{10}$/)
+  phoneNumber!: string;
+
+  @IsOptional()
+  @IsString()
+  @Matches(/^[0-9]{10}$/)
+  alternatePhoneNumber?: string;
+
+  @IsOptional()
+  @IsBoolean()
+  status?: boolean;
+
+  @IsOptional()
+  @Type(() => createAddressDto)
+  address?: createAddressDto;
+
+  @IsDateString()
+  dateOfBirth!: string;
+
+  @IsEmail()
+  email!: string;
+
+  @IsOptional()
+  @IsEmail()
+  alternateEmail?: string;
+
+  @IsString()
+  @MinLength(8, { message: 'Password must be at least 8 characters' })
+  password!: string;
+}
