605e0d62fff4f461252948bc36fcec15d72f036d
Nilesh Pratap Singh <nileshpratap190902@gmail.com>
2026-07-22 17:47:08 +0530

    Admin controller implemented.

--- src/admin/admin.controller.ts
+++ src/admin/admin.controller.ts
@@ -1,34 +1,50 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+    Controller,
+    Get,
+    Post,
+    Body,
+    Patch,
+    Param,
+    Delete,
+    UseGuards,
+} from '@nestjs/common';
 import { AdminService } from './admin.service';
 import { CreateAdminDto } from './dto/create-admin.dto';
 import { UpdateAdminDto } from './dto/update-admin.dto';
-
+import { ApiGuard } from '@guards/api-guard/api-guard.guard';
 @Controller('admin')
 export class AdminController {
-  constructor(private readonly adminService: AdminService) {}
+    constructor(private readonly adminService: AdminService) { }
 
-  @Post()
-  create(@Body() createAdminDto: CreateAdminDto) {
-    return this.adminService.create(createAdminDto);
-  }
+    @UseGuards(ApiGuard)
+    @Get()
+    findAll() {
+        return this.adminService.findAll();
+    }
 
-  @Get()
-  findAll() {
-    return this.adminService.findAll();
-  }
+    @Get(':id')
+    findOne(@Param('id') id: string) {
+        console.log("id type", typeof (id));
+        return this.adminService.findOne(id);
+    }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.adminService.findOne(+id);
-  }
+    @Patch(':id')
+    update(
+        @Param('id') id: string,
+        @Body() updateAdminDto: UpdateAdminDto
+    ) {
+        return this.adminService.update(id, updateAdminDto);
+    }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateAdminDto: UpdateAdminDto) {
-    return this.adminService.update(+id, updateAdminDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.adminService.remove(+id);
-  }
+    @Delete(':id')
+    remove(@Param('id') id: string) {
+        return this.adminService.remove(id);
+    }
+    @Patch('toggle/:id')
+    addButton(
+        @Param(':id')
+        id: string
+    ) {
+        return this.adminService.addButton(id)
+    }
 }
