1632410970561b4985ef05b535ae40d98fac6d16
Nilesh Pratap Singh <nileshpratap190902@gmail.com>
2026-09-23 02:36:57 +0530

    Maintainance

--- src/common/guards/login-protected/login-protected.guard.ts
+++ src/common/guards/login-protected/login-protected.guard.ts
@@ -1,7 +1,9 @@
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 import { Observable } from 'rxjs';
 import { type Request } from 'express';
-import { type TokenPayload, TokenUtility } from '@utils/token.utility';
+import { type TokenPayload } from '@custom_types/tokenPayload.types';
+import { TokenUtility } from '@utils/token.utility';
+
 @Injectable()
 export class LoginProtectedGuard implements CanActivate {
     constructor(
@@ -12,8 +14,8 @@ export class LoginProtectedGuard implements CanActivate {
     ): boolean | Promise<boolean> | Observable<boolean> {
         const req:Request = context.switchToHttp().getRequest();
         const token:string |null = this.tokenUtility.getToken(req);
-        const decoded:TokenPayload = this.tokenUtility.verifyToken(token as string);
-        if(!decoded.role&&decoded) return true;
-        return false;
+        const decoded:TokenPayload|null = this.tokenUtility.verifyToken(token as string);
+        if(!decoded) return false;
+        return true;
     }
 }
--- src/common/guards/login-restriction/login-restriction.guard.ts
+++ src/common/guards/login-restriction/login-restriction.guard.ts
@@ -1,11 +1,35 @@
-import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
-import { Observable } from 'rxjs';
+import {
+    CanActivate,
+    ExecutionContext,
+    Injectable,
+    UnauthorizedException,
+} from '@nestjs/common';
+import { TokenUtility } from '@utils/token.utility';
+import { Request } from 'express';
 
 @Injectable()
 export class LoginRestrictionGuard implements CanActivate {
-  canActivate(
-    context: ExecutionContext,
-  ): boolean | Promise<boolean> | Observable<boolean> {
-    return true;
-  }
+    constructor(
+        private readonly tokenUtility:TokenUtility
+    ){}
+    canActivate(context: ExecutionContext): boolean {
+        const req:Request = context.switchToHttp().getRequest<Request>();
+
+        const authHeader = req.headers.authorization;
+
+        const refreshToken =
+            req.cookies?.refreshToken ??
+                (authHeader?.startsWith('Bearer ')
+                    ? authHeader.substring(7)
+                    : authHeader);
+
+        if (!refreshToken) {
+            throw new UnauthorizedException('Refresh token not found');
+        }
+
+        const verify = this.tokenUtility.verifyToken(refreshToken);
+        if(!verify) return false;
+
+        return true;
+    }
 }
